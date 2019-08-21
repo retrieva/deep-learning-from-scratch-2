ca133cc8e17df5ee9a45604f2849547ca6d485cd
Hiroki Iida <hiroki.iida@retrieva.jp>
2019-08-21 20:59:12 +0900

    fix to_cpu for gpu execution

--- gpu/common/util.py
+++ gpu/common/util.py
@@ -1,11 +1,12 @@
 import numpy as np
+import cupy as cp
 
 
 def to_cpu(x):
     import numpy
     if type(x) == numpy.ndarray:
         return x
-    return np.asnumpy(x)
+    return cp.asnumpy(x)
 
 
 def to_gpu(x):
