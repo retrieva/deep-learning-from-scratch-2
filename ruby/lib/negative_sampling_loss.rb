e4bff16190e188f0e20794c0a4adb8784fcdda32
Daisuke Shimamoto <diskshima@gmail.com>
2019-08-25 00:50:57 +0900

    Add weighted random sampling w/o replacement for Ruby

--- ruby/lib/negative_sampling_loss.rb
+++ ruby/lib/negative_sampling_loss.rb
@@ -38,12 +38,62 @@ class UnigramSampler
       target_idx = target[i]
       p[target_idx] = 0
       p /= p.sum
-      negative_sample[i, true] # = np.random.choiceに代わるもの
+      negative_sample[i, true] = random_choice_without_replacement(
+        @vocab_size, size: @sample_size, p: p, replacement: false
+      )
     end
   end
 end
 
-# With Replacement なので、要追加実装
-def wrs(freq)
-  freq.max_by { |_, weight| rand ** (1.0 / weight) }.first }
+# Implementation is based on the Weighted Random Sampling from this SO
+# https://stackoverflow.com/a/2149533.
+def random_choice_without_replacement(a, size: 1, p:)
+  array = a.class == Integer ? (0...a).to_a : a
+  items = array.zip(p)
+
+  heap = rws_heap(items)
+
+  size.times.map { rws_heap_pop(heap) }
+end
+
+Node = Struct.new(:w, :v, :tw)
+Rand = Random.new
+
+def rws_heap(items)
+  h = [nil]
+  items.each do |w, v|
+    h.append(Node.new(w, v, w))
+  end
+
+  (h.length - 1).downto(2).each do |i|
+    h[i >> 1].tw += h[i].tw
+  end
+
+  h
+end
+
+def rws_heap_pop(h)
+  gas = h[1].tw * Rand.rand
+
+  i = 1
+
+  while gas >= h[i].w
+    gas -= h[i].w
+    i <<= 1
+    if gas >= h[i].tw
+      gas -= h[i].tw
+      i += 1
+    end
+  end
+
+  w = h[i].w
+  v = h[i].v
+
+  h[i].w = 0
+  while i.positive?
+    h[i].tw -= w
+    i >>= 1
+  end
+
+  v
 end
