52b3cc9f236b482cab39adad92ef8d0501e800bc
MISUMI Masaru <deltam@users.noreply.github.com>
2019-12-08 15:07:29 +0900

    Add LSTM, unfinished

--- python/common/time_layers.py
+++ python/common/time_layers.py
@@ -198,8 +198,42 @@ class TimeSoftmaxWithLoss:
         return dx
 
 
+class LSTM:
+    def __init__(self, Wx, Wh, b):
+        self.params = [Wx, Wh, b]
+        self.grads = [np.zeros_like(Wx), np.zeros_like(Wh), np.zeros_like(b)]
+        self.cache = None
 
+    def forward(self, x, h_prev, c_prev):
+        Wx, Wh, b = self.params
+        N,H = h_prev.shape
 
+        A = np.dot(x, Wx) + np.dot(h_prev,Wh) + b
 
+        f = A[:, :H]
+        g = A[:, H:2*H]
+        i = A[:, 2*H:3*H]
+        o = A[:, 3*H:]
 
-    
+        f = sigmoid(f)
+        g = np.tanh(g)
+        i = sigmoid(i)
+        o = sigmoid(o)
+
+        c_next = f * c_prev + g*i
+        h_next = o * np.tanh(c_next)
+
+        self.cache = (x, h_prev, c_prev, i, f, g, o, c_next)
+        return h_next, c_next
+
+    def backward(self, dx, dh_next, dc_next):
+        Wx, Wh, b = self.params
+        N,H = h_prev.shape
+        x, h_prev, c_prev, i, f, g, o, c_next = self.cache
+
+        do = dh_next * np.tanh(c_next)
+
+        df =
+        dc_prev =
+        dg =
+        di =
