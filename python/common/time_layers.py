787759003c956ec1cbb99747149e974b8ce30e5e
MISUMI Masaru <deltam@users.noreply.github.com>
2019-12-19 01:17:45 +0900

    [Python] Add TimeLSTM layer

--- python/common/time_layers.py
+++ python/common/time_layers.py
@@ -261,3 +261,64 @@ class LSTM:
         dh_prev = np.dot(dA, Wh.T)
 
         return dx, dh_prev, dc_prev
+
+
+class TimeLSTM:
+    def __init__(self, Wx, Wh, b, stateful=false):
+        self.params = [Wx, Wh, b]
+        self.grads = [np.zeros_like(Wx), np.zeros_like(Wh), np.zeros_like(b)]
+        self.layers = None
+
+        self.h, self.c = None, None
+        self.dh = None
+        self.stateful = stateful
+
+    def forward(self, xs):
+        Wx, Wh, b = self.params
+        N, T, D = xs.shape
+        H = Wh.shape[0]
+
+        self.layers = []
+        hs = np.empty((N,T,H), dtype='f')
+
+        if not self.stateful or self.h is None:
+            self.h = np.zeros((N,H), dtype='f')
+        if not self.stateful or self.c is None:
+            self.c = np.zeros((N,H), dtype='f')
+
+        for t in range(T):
+            layer = LSTM(*self.params)
+            self.h, self.c = layer.forward(xs[:,t,:], self.h, self.c)
+            hs[:,t,:] = self.h
+
+            self.layers.append(layer)
+
+        return hs
+
+    def backward(self, dhs):
+        Wx, Wh, b = self.params
+        N, T, H = dhs.shape
+        D = Wx.shape[0]
+
+        dxs = np.empty((N,T,D), dtype='f')
+        dh, dc = 0, 0
+
+        grads = [0,0,0]
+        for t in reversed(range(T)):
+            layer = self.layers[t]
+            dx, dh, dc = layer.backward(dhs[:, t, :] + dh, dc)
+            dxs[:,t,:] = dx
+            for i, grad in enumerate(layer.grads):
+                grads[i] += grad
+
+        for i, grad in enumerate(grads):
+            self.grads[i][...] = grad
+
+        self.dh = dh
+        return dxs
+
+    def set_state(self, h, c=None):
+        self.h, self.c = h, c
+
+    def reset_state(self):
+        self.h, self.c = None, None
