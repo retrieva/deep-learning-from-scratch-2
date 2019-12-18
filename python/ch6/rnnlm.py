41c14121ec2a8c5a7177f2a4c2902b6f0e5fe3aa
MISUMI Masaru <deltam@users.noreply.github.com>
2019-12-19 01:18:41 +0900

    [Python] Add Rnnlm, forward

---
+++ python/ch6/rnnlm.py
@@ -0,0 +1,31 @@
+import sys
+sys.path.append('..')
+import numpy as np
+from common.time_layers import *
+import pickle
+
+
+class Rnnlm:
+    def __init__(self, vocab_size=10000, wordvec_size=100, hidden_size=100):
+        V, D, H = vocab_size, wordvec_size, hidden_size
+        rn = np.random.randn
+
+        # 重みの初期化
+        embed_W = (rn(V, D) / 100).astype('f')
+        lstm_Wx = (rn(D, 4*H) / np.sqrt(D)).astype('f') # Xavierの初期値
+        lstm_Wh = (rn(H, 4*H) / np.sqrt(H)).astype('f')
+        lstm_b = np.zeros(4*H).astype('f')
+        affine_W = (rn(H,V) / np.sqrt(H)).astype('f')
+        affine_b = np.zeros(V).astype('f')
+
+        # レイヤの生成
+        self.layers = [
+            TimeEmbedding(embed_W),
+            TimeLSTM(lstm_Wx, lstm_Wh, lstm_b, stateful=True),
+            TimeAffine(affine_W, affine_b)
+        ]
+        self.loss_layer = TimeSoftmaxWithLoss()
+        self.lstm_layer = self.layers[1]
+
+        self.params, self.grads = [],[]
+        for layer in self.layers
