9803c4457a9d3a57a00563a06da3aa2aeb56c7eb
MISUMI Masaru <deltam@users.noreply.github.com>
2020-01-23 08:43:25 +0900

    [Python] Add train_rannlm

--- python/common/util.py
+++ python/common/util.py
@@ -1,4 +1,5 @@
 import numpy as np
+import sys
 
 def preprocess(text):
     text = text.lower()
@@ -29,7 +30,7 @@ def create_contexts_target(corpus, window_size=1):
             cs.append(corpus[idx+t])
         contexts.append(cs)
 
-    return np.array(contexts), np.array(target) 
+    return np.array(contexts), np.array(target)
 
 def convert_one_hot(corpus, vocab_size):
     '''one-hot表現への変換
@@ -64,6 +65,48 @@ def clip_grads(grads, max_norm):
         for grad in grads:
             grad *= rate
 
+def eval_perplexity(model, corpus, batch_size=10, time_size=35):
+    print('evaluating perplexity ...')
+    corpus_size = len(corpus)
+    total_loss, loss_cnt = 0, 0
+    max_iters = (corpus_size - 1) // (batch_size * time_size)
+    jump = (corpus_size - 1) // batch_size
+
+    for iters in range(max_iters):
+        xs = np.zeros((batch_size, time_size), dtype=np.int32)
+        ts = np.zeros((batch_size, time_size), dtype=np.int32)
+        time_offset = iters * time_size
+        offsets = [time_offset + (i * jump) for i in range(batch_size)]
+        for t in range(time_size):
+            for i, offset in enumerate(offsets):
+                xs[i, t] = corpus[(offset + t) % corpus_size]
+                ts[i, t] = corpus[(offset + t + 1) % corpus_size]
+
+        try:
+            loss = model.forward(xs, ts, train_flg=False)
+        except TypeError:
+            loss = model.forward(xs, ts)
+        total_loss += loss
+
+        sys.stdout.write('\r%d / %d' % (iters, max_iters))
+        sys.stdout.flush()
+
+    print('')
+    ppl = np.exp(total_loss / max_iters)
+    return ppl
+
+def to_cpu(x):
+    import numpy
+    if type(x) == numpy.ndarray:
+        return x
+    return np.asnumpy(x)
+
+
+def to_gpu(x):
+    import cupy
+    if type(x) == cupy.ndarray:
+        return x
+    return cupy.asarray(x)
 
 """
 # coding: utf-8
