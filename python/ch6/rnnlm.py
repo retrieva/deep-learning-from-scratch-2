8b11d9ddf322ea5fd52a104320e6ba1dc16706ac
MISUMI Masaru <deltam@users.noreply.github.com>
2020-01-23 08:42:37 +0900

    [Python] Add Rnnlm

--- python/ch6/rnnlm.py
+++ python/ch6/rnnlm.py
@@ -27,5 +27,39 @@ class Rnnlm:
         self.loss_layer = TimeSoftmaxWithLoss()
         self.lstm_layer = self.layers[1]
 
+        # すべての重みと勾配をリストにまとめる
         self.params, self.grads = [],[]
-        for layer in self.layers
+        for layer in self.layers:
+            self.params += layer.params
+            self.grads += layer.grads
+
+    def predict(self, xs):
+        for layer in self.layers:
+            xs = layer.forward(xs)
+        return xs
+
+    def forward(self, xs, ts):
+        score = self.predict(xs)
+        loss = self.loss_layer.forward(score, ts)
+        return loss
+
+    def backward(self, dout=1):
+        dout = self.loss_layer.backward(dout)
+        for layer in reversed(self.layers):
+            dout = layer.backward(dout)
+        return dout
+
+    def reset_state(self):
+        self.lstm_layer.reset_state()
+
+    def save_params(self, file_name='Rnnlm.pkl'):
+        with open(file_name, 'wb') as f:
+            pickle.dump(self.params, f)
+
+    def load_params(self, file_name='Rnnlm.pkl'):
+        with open(file_name, 'rb') as f:
+            prms = pickle.load(f)
+
+        # 教科書には無いが、これがないと読み込めていなかった
+        for i, param in enumerate(self.params):
+            param[...] = prms[i]
