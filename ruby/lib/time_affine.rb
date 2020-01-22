6e55a2e63368229803c1d7e37569e10016e7354c
Daisuke Shimamoto <diskshima@gmail.com>
2020-01-22 23:37:13 +0900

    TimeAffineにテンソルの次元数を追記

--- ruby/lib/time_affine.rb
+++ ruby/lib/time_affine.rb
@@ -3,29 +3,29 @@ require 'numo/narray'
 class TimeAffine
   attr_accessor :params, :grads
 
-  def initialize(w, b)
+  def initialize(w, b) # w: d x v, b: v
     @params = [w, b]
     @grads = [w.new_zeros, b.new_zeros]
   end
 
-  def forward(x)
+  def forward(x)                # x: n x t x d
     n, t, _d = x.shape
     w, b = @params
 
-    rx = x.reshape(n * t, true)
-    out = rx.dot(w) + b
+    rx = x.reshape(n * t, true) # (n*t) x d
+    out = rx.dot(w) + b         # (n*t) x v
     @x = x
-    out.reshape(n, t, true)
+    out.reshape(n, t, true)     #  n x t x v
   end
 
   def backward(dout)
-    n, t, _hv = dout.shape
+    n, t, _v = dout.shape
     w, _b = @params
-    rdout = dout.reshape(n * t, true)  # rdout: n*t x (h x v)
+    rdout = dout.reshape(n * t, true)                 # (n*t) x v
 
-    db = rdout.sum(axis: 0)   # db: (h x v)
-    dw = @x.reshape(n * t, true).transpose.dot(rdout)
-    dx = rdout.dot(w.transpose).reshape(*@x.shape)   # w: (h x v)
+    db = rdout.sum(axis: 0)                           # v
+    dw = @x.reshape(n * t, true).transpose.dot(rdout) # d x v
+    dx = rdout.dot(w.transpose).reshape(*@x.shape)    # n x t x d
 
     @grads[0].store dw
     @grads[1].store db
